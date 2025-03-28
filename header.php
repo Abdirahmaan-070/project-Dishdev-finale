5913655fda38e43e993350f09efee871f3aaff77
Abdirahmaan <6045920@mborijnland.nl>
2025-03-28 12:15:08 +0100

    header toegevoegd
    header toegevoegd

---
+++ header.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Abdirahmaan">
+    <link rel="stylesheet" href="CSS/style.css">
+    <script defer src="Script.JS"></script>
+    <title>Header en Footer</title>
+</head>
+
+<body>
+    <header>
+        <img id="Malo" src="Images/logo_vlam_en_vlees.png" alt="Logo1">
+
+        <nav>
+            <ul class="nav-links">
+                <li><a href="LandingsPagina.php">Home</a></li>
+                <li><a href="Lucnh en Diner">Menu</a></li>
+                <li><a href="Openingstijden en Locatie.php">Openingstijden en Locatie</a></li>
+                <li><a href="Reservering.php">Reservering</a></li>
+                <li><a href="Vactures.php">Vactures</a></li>
+            </ul>
+            <div class="menu-icon" onclick="toggleMenu()">☰</div>
+        </nav>
+
+        <ul class="mobile-menu">
+            <li><a href="LandingsPagina.php">Home</a></li>
+            <li><a href="Lucnh en Diner">Menu</a></li>
+            <li><a href="Openingstijden en Locatie.php">Openingstijden en Locatie</a></li>
+            <li><a href="Reservering.php">Reservering</a></li>
+            <li><a href="Vactures.php">Vactures</a></li>
+        </ul>
+    </header>
+</body>
+
+</html>
+
